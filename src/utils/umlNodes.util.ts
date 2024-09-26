7c80340bedeb61ef3001003fadc142e039cb9229
Szobonya Dávid <szobonya.david@gmail.com>
2024-09-26 12:28:16 +0200

    Operation in progress

--- src/utils/umlNodes.util.ts
+++ src/utils/umlNodes.util.ts
@@ -21,46 +21,28 @@ export enum Visibility {
     PACKAGE='~'
 }
 
+export type Direction = 'in'|'out'|'inout';
+
 /**
+ * Member of Property.
+ *
  * Based on chapter 7.5.4.1 of UML 2.5.1 specification.
  *
- * <multiplicity> ::= <multiplicity-range> [ [ ‘{‘ <order-designator> [‘,’ <uniqueness-designator> ] ‘}’ ] |
- *  [ ‘{‘ <uniqueness-designator> [‘,’ <order-designator> ] ‘}’ ] ]
+ *  [ <lower> ‘..’ ] <upper>
  */
-export class Multiplicity {
-    lower: number|null;
-    upper: number|'*';
-    orderDesignation: boolean|null;
-    uniquenessDesignation: boolean|null
+export class MultiplicityRange {
+    private readonly _lower: number|null;
+    private readonly _upper: number|'*';
 
     constructor(upper: number|'*',
-                lower: number|null = null,
-                orderDesignation: boolean|null = null,
-                uniquenessDesignation: boolean|null = null) {
-        this.lower = lower;
-        this.upper = upper;
-        this.orderDesignation = orderDesignation;
-        this.uniquenessDesignation = uniquenessDesignation;
+                lower: number|null = null) {
+        this._lower = lower;
+        this._upper = upper;
     }
 
     toString(): string {
-        let value = `[${this.lower ? this.lower + '..' : ''}${this.upper}]`;
-
-        const designations = [];
-        if (this.orderDesignation !== null) {
-            designations.push(this.orderDesignation ? 'ordered' : 'unordered');
-        }
-        if (this.uniquenessDesignation !== null) {
-            designations.push(this.uniquenessDesignation ? 'unique' : 'nonunique');
-        }
-
-        if (designations.length > 0) {
-            value += ` {${designations.join(' ')}}`;
-        }
-
-        return value;
+        return `${this._lower ? this._lower + '..' : ''}${this._upper}`;
     }
-
 }
 
 /**
@@ -70,37 +52,53 @@ export class Multiplicity {
  *  [‘,’ <prop-modifier >]* ’}
  */
 export class Property {
-    visibility: Visibility|null;
-    isDerived: boolean;
-    name: string;
-    type: string|null;
-    multiplicity: Multiplicity|null;
-    defaultValue: string|null;
+    private readonly _visibility: Visibility|null;
+    private readonly _isDerived: boolean;
+    private readonly _name: string;
+    private readonly _type: string|null;
+    private readonly _multiplicity: MultiplicityRange|null;
+    private readonly _defaultValue: string|null;
     // TODO: implement modifiers
 
     constructor(name: string,
                 type: string|null = null,
                 visibility: Visibility|null = null,
                 isDerived: boolean = false,
-                multiplicity: Multiplicity|null = null,
+                multiplicity: MultiplicityRange|null = null,
                 defaultValue: string|null = null) {
-        this.visibility = visibility;
-        this.name = name;
-        this.type = type;
-        this.isDerived = isDerived;
-        this.multiplicity = multiplicity;
-        this.defaultValue = defaultValue;
+        this._visibility = visibility;
+        this._name = name;
+        this._type = type;
+        this._isDerived = isDerived;
+        this._multiplicity = multiplicity;
+        this._defaultValue = defaultValue;
     }
 
     toString(): string {
-        let value = `${this.visibility ?? ''}${this.isDerived ? '/' : ''}${this.name}`;
+        let value = `${this._visibility ?? ''}${this._isDerived ? '/' : ''}${this._name}`;
 
-        if (this.type) value += `: ${this.type}`;
-        if (this.multiplicity) value += this.multiplicity.toString();
-        if (this.defaultValue) value += ` = ${this.defaultValue}`;
+        if (this._type) value += `: ${this._type}`;
+        if (this._multiplicity) value += `[${this._multiplicity.toString()}]`;
+        if (this._defaultValue) value += ` = ${this._defaultValue}`;
 
         return value;
     }
+
+}
+
+/**
+ * Member of Operation.
+ *
+ * Based on chapter 9.4.4 of UML 2.5.1 specification.
+ *
+ * [<direction>] <parameter-name> ’:’ <type-expression> [’[’<multiplicity-range>’]’] [’=’ <default>]
+ * [’{’ <parm-property> [’,’ <parm-property>]* ’}’]
+ */
+export class Parameter {
+    private _direction: Direction|null;
+    private _name: string;
+    private _type: string;
+    private _multiplicity: MultiplicityRange|null;
 
 }
 
@@ -111,8 +109,8 @@ export class Property {
  *  [‘{‘ <oper-property> [‘,’ <oper-property>
  */
 export class Operation {
-    visibility: Visibility|null;
-    name: string;
+    private readonly visibility: Visibility|null;
+    private readonly name: string;
     //TODO: finish
 }
 
