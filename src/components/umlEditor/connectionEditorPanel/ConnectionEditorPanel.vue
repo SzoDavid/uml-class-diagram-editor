7569ec35b5f37f1233a47d949dae2cb15f870280
Szobonya Dávid <szobonya.david@gmail.com>
2024-12-03 20:10:57 +0100

    styled connection

--- src/components/umlEditor/connectionEditorPanel/ConnectionEditorPanel.vue
+++ src/components/umlEditor/connectionEditorPanel/ConnectionEditorPanel.vue
@@ -59,144 +59,121 @@
               </template>
 
               <v-card :title="t('multiplicity')" variant="outlined">
+                <v-text-field :label="t('multiplicity_upper')"
+                              v-model="data.instance.startMultiplicity.upper"
+                              :rules="[() => t(getError({parameter: 'startMultiplicity', child: {parameter: 'upper'}})) ?? true]"
+                              type="text" />
 
+                <v-text-field :label="t('multiplicity_lower')"
+                              v-model="data.instance.startMultiplicity.lower"
+                              :rules="[() => t(getError({parameter: 'startMultiplicity', child: {parameter: 'lower'}})) ?? true]"
+                              type="number" />
               </v-card>
             </v-card-text>
           </v-card>
 
+          <!-- End point -->
+          <v-card :title="t('end_point')" variant="outlined">
+            <v-card-text>
+              <v-text-field :label="t('name')"
+                            v-model="data.instance.endName"
+                            :rules="[() => t(getError({parameter: 'endName'})) ?? true]"
+                            type="text" />
+
+              <template v-if="data.instance instanceof Aggregation">
+                <v-checkbox :label="t('shared')" v-model="data.instance.isEndShared" />
+              </template>
+              <template v-else-if="data.instance instanceof Association">
+                <v-select :label="t('navigability.title')" v-model="data.instance.endNavigability" :items="[
+                  { title: t('navigability.unspecified'), value: AssociationNavigability.UNSPECIFIED },
+                  { title: t('navigability.navigable'), value: AssociationNavigability.NAVIGABLE },
+                  { title: t('navigability.unnavigable'), value: AssociationNavigability.UNNAVIGABLE }
+                ]" />
+              </template>
+
+              <v-card :title="t('multiplicity')" variant="outlined">
+                <v-text-field :label="t('multiplicity_upper')"
+                              v-model="data.instance.endMultiplicity.upper"
+                              :rules="[() => t(getError({parameter: 'endMultiplicity', child: {parameter: 'upper'}})) ?? true]"
+                              type="text" />
+
+                <v-text-field :label="t('multiplicity_lower')"
+                              v-model="data.instance.endMultiplicity.lower"
+                              :rules="[() => t(getError({parameter: 'endMultiplicity', child: {parameter: 'lower'}})) ?? true]"
+                              type="number" />
+              </v-card>
+            </v-card-text>
+          </v-card>
         </template>
       </template>
-      <template v-else-if="data.instance instanceof ConnectionPoint"></template>
+      <template v-else-if="data.instance instanceof ConnectionPoint && (
+        data.instance.parent instanceof Aggregation ||
+        data.instance.parent instanceof Association ||
+        data.instance.parent instanceof Composition)">
+        <template v-if="data.instance.isStartPoint">
+          <v-text-field :label="t('name')"
+                        v-model="data.instance.parent.startName"
+                        :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'startName'}})) ?? true]"
+                        type="text" />
+
+          <template v-if="data.instance.parent instanceof Aggregation">
+            <v-checkbox :label="t('shared')" v-model="data.instance.parent.isStartShared" />
+          </template>
+          <template v-else-if="data.instance.parent instanceof Association">
+            <v-select :label="t('navigability.title')" v-model="data.instance.parent.startNavigability" :items="[
+              { title: t('navigability.unspecified'), value: AssociationNavigability.UNSPECIFIED },
+              { title: t('navigability.navigable'), value: AssociationNavigability.NAVIGABLE },
+              { title: t('navigability.unnavigable'), value: AssociationNavigability.UNNAVIGABLE }
+            ]" />
+          </template>
+
+          <v-card :title="t('multiplicity')" variant="outlined">
+            <v-card-text>
+              <v-text-field :label="t('multiplicity_upper')"
+                            v-model="data.instance.parent.startMultiplicity.upper"
+                            :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'startMultiplicity', child: {parameter: 'upper'}}})) ?? true]"
+                            type="text" />
+
+              <v-text-field :label="t('multiplicity_lower')"
+                            v-model="data.instance.parent.startMultiplicity.lower"
+                            :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'startMultiplicity', child: {parameter: 'lower'}}})) ?? true]"
+                            type="number" />
+            </v-card-text>
+          </v-card>
+        </template>
+        <template v-else-if="data.instance.isEndpoint">
+          <v-text-field :label="t('name')"
+                        v-model="data.instance.parent.endName"
+                        :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'endName'}})) ?? true]"
+                        type="text" />
+
+          <template v-if="data.instance.parent instanceof Aggregation">
+            <v-checkbox :label="t('shared')" v-model="data.instance.parent.isEndShared" />
+          </template>
+          <template v-else-if="data.instance.parent instanceof Association">
+            <v-select :label="t('navigability.title')" v-model="data.instance.parent.endNavigability" :items="[
+              { title: t('navigability.unspecified'), value: AssociationNavigability.UNSPECIFIED },
+              { title: t('navigability.navigable'), value: AssociationNavigability.NAVIGABLE },
+              { title: t('navigability.unnavigable'), value: AssociationNavigability.UNNAVIGABLE }
+            ]" />
+          </template>
+
+          <v-card :title="t('multiplicity')" variant="outlined">
+            <v-card-text>
+              <v-text-field :label="t('multiplicity_upper')"
+                            v-model="data.instance.parent.endMultiplicity.upper"
+                            :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'endMultiplicity', child: {parameter: 'upper'}}})) ?? true]"
+                            type="text" />
+
+              <v-text-field :label="t('multiplicity_lower')"
+                            v-model="data.instance.parent.endMultiplicity.lower"
+                            :rules="[() => t(getError({parameter: 'parent', child: {parameter: 'endMultiplicity', child: {parameter: 'lower'}}})) ?? true]"
+                            type="number" />
+            </v-card-text>
+          </v-card>
+        </template>
+      </template>
     </v-expansion-panel-text>
   </v-expansion-panel>
   <v-btn @click="onSave">{{ t("save") }}</v-btn>
-  <!--  <fieldset>-->
-  <!--    <legend class="capitalized">{{ t("detail", 2) }}</legend>-->
-  <!--    <template v-else-if="data.instance instanceof Association-->
-  <!--      || data.instance instanceof Aggregation-->
-  <!--      || data.instance instanceof Generalization-->
-  <!--      || data.instance instanceof Composition">-->
-  <!--      -->
-  <!--      <fieldset v-if="!(data.instance instanceof Generalization)">-->
-  <!--        <legend class="capitalized">{{ t("start_point") }}</legend>-->
-
-
-  <!--        <fieldset>-->
-  <!--          <legend class="capitalized">{{ t("multiplicity") }}</legend>-->
-  <!--          <div class="grid-form">-->
-  <!--            <label for="startMultiUpper" class="capitalized">{{ t("multiplicity_upper") }}</label>-->
-  <!--            <input id="startMultiUpper" type="text" v-model="data.instance['startMultiplicity'].upper">-->
-
-  <!--            <label for="startMultiLower" class="capitalized">{{ t("multiplicity_lower") }}</label>-->
-  <!--            <input id="startMultiLower" type="number" v-model="data.instance['startMultiplicity'].lower">-->
-  <!--          </div>-->
-  <!--        </fieldset>-->
-  <!--      </fieldset>-->
-  <!--      <fieldset v-if="!(data.instance instanceof Generalization)">-->
-  <!--        <legend class="capitalized">{{ t("end_point") }}</legend>-->
-
-  <!--        <div class="grid-form">-->
-  <!--          <label for="endName" class="capitalized">{{ t("name") }}</label>-->
-  <!--          <input id="endName" type="text" v-model="data.instance['endName']">-->
-
-  <!--          <template v-if="data.instance instanceof Aggregation">-->
-  <!--            <label for="endShared" class="capitalized">{{ t("shared") }}</label>-->
-  <!--            <input id="endShared" type="checkbox" v-model="data.instance.isEndShared">-->
-  <!--          </template>-->
-
-  <!--          <template v-if="data.instance instanceof Association">-->
-  <!--            <label for="endNavigability" class="capitalized">{{ t("navigability.title") }}</label>-->
-  <!--            <select id="endNavigability" v-model="data.instance.endNavigability">-->
-  <!--              <option :value="AssociationNavigability.UNSPECIFIED">{{ t("navigability.unspecified") }}</option>-->
-  <!--              <option :value="AssociationNavigability.NAVIGABLE">{{ t("navigability.navigable") }}</option>-->
-  <!--              <option :value="AssociationNavigability.UNNAVIGABLE">{{ t("navigability.unnavigable") }}</option>-->
-  <!--            </select>-->
-  <!--          </template>-->
-  <!--        </div>-->
-
-  <!--        <fieldset>-->
-  <!--          <legend class="capitalized">{{ t("multiplicity") }}</legend>-->
-  <!--          <div class="grid-form">-->
-  <!--            <label for="endMultiUpper" class="capitalized">{{ t("multiplicity_upper") }}</label>-->
-  <!--            <input id="endMultiUpper" type="text" v-model="data.instance['endMultiplicity'].upper">-->
-
-  <!--            <label for="endMultiLower" class="capitalized">{{ t("multiplicity_lower") }}</label>-->
-  <!--            <input id="endMultiLower" type="number" v-model="data.instance['endMultiplicity'].lower">-->
-  <!--          </div>-->
-  <!--        </fieldset>-->
-  <!--      </fieldset>-->
-  <!--    </template>-->
-  <!--    <template v-if="data.instance instanceof ConnectionPoint">-->
-  <!--      <template v-if="data.instance.isStartPoint && (-->
-  <!--        data.instance.parent instanceof Aggregation ||-->
-  <!--        data.instance.parent instanceof Association ||-->
-  <!--        data.instance.parent instanceof Composition)">-->
-
-  <!--        <div class="grid-form">-->
-  <!--          <label for="startName" class="capitalized">{{ t("name") }}</label>-->
-  <!--          <input id="startName" type="text" v-model="data.instance.parent['startName']">-->
-
-  <!--          <template v-if="data.instance.parent instanceof Aggregation">-->
-  <!--            <label for="startShared" class="capitalized">{{ t("shared") }}</label>-->
-  <!--            <input id="startShared" type="checkbox" v-model="data.instance.parent.isStartShared">-->
-  <!--          </template>-->
-
-  <!--          <template v-if="data.instance.parent instanceof Association">-->
-  <!--            <label for="startNavigability" class="capitalized">{{ t("navigability.title") }}</label>-->
-  <!--            <select id="startNavigability" v-model="data.instance.parent.startNavigability">-->
-  <!--              <option :value="AssociationNavigability.UNSPECIFIED">{{ t("navigability.unspecified") }}</option>-->
-  <!--              <option :value="AssociationNavigability.NAVIGABLE">{{ t("navigability.navigable") }}</option>-->
-  <!--              <option :value="AssociationNavigability.UNNAVIGABLE">{{ t("navigability.unnavigable") }}</option>-->
-  <!--            </select>-->
-  <!--          </template>-->
-  <!--        </div>-->
-
-  <!--        <fieldset>-->
-  <!--          <legend class="capitalized">{{ t("multiplicity") }}</legend>-->
-  <!--          <div class="grid-form">-->
-  <!--            <label for="startMultiUpper" class="capitalized">{{ t("multiplicity_upper") }}</label>-->
-  <!--            <input id="startMultiUpper" type="text" v-model="data.instance.parent['startMultiplicity'].upper">-->
-
-  <!--            <label for="startMultiLower" class="capitalized">{{ t("multiplicity_lower") }}</label>-->
-  <!--            <input id="startMultiLower" type="number" v-model="data.instance.parent['startMultiplicity'].lower">-->
-  <!--          </div>-->
-  <!--        </fieldset>-->
-  <!--      </template>-->
-  <!--      <template v-else-if="data.instance.isEndpoint && (-->
-  <!--        data.instance.parent instanceof Aggregation ||-->
-  <!--        data.instance.parent instanceof Association ||-->
-  <!--        data.instance.parent instanceof Composition)">-->
-
-  <!--        <div class="grid-form">-->
-  <!--          <label for="endName" class="capitalized">{{ t("name") }}</label>-->
-  <!--          <input id="endName" type="text" v-model="data.instance.parent['endName']">-->
-
-  <!--          <template v-if="data.instance.parent instanceof Aggregation">-->
-  <!--            <label for="endShared" class="capitalized">{{ t("shared") }}</label>-->
-  <!--            <input id="endShared" type="checkbox" v-model="data.instance.parent.isEndShared">-->
-  <!--          </template>-->
-
-  <!--          <template v-if="data.instance.parent instanceof Association">-->
-  <!--            <label for="endNavigability" class="capitalized">{{ t("navigability.title") }}</label>-->
-  <!--            <select id="endNavigability" v-model="data.instance.parent.endNavigability">-->
-  <!--              <option :value="AssociationNavigability.UNSPECIFIED">{{ t("navigability.unspecified") }}</option>-->
-  <!--              <option :value="AssociationNavigability.NAVIGABLE">{{ t("navigability.navigable") }}</option>-->
-  <!--              <option :value="AssociationNavigability.UNNAVIGABLE">{{ t("navigability.unnavigable") }}</option>-->
-  <!--            </select>-->
-  <!--          </template>-->
-  <!--        </div>-->
-
-  <!--        <fieldset>-->
-  <!--          <legend class="capitalized">{{ t("multiplicity") }}</legend>-->
-  <!--          <div class="grid-form">-->
-  <!--            <label for="endMultiUpper" class="capitalized">{{ t("multiplicity_upper") }}</label>-->
-  <!--            <input id="endMultiUpper" type="text" v-model="data.instance.parent['endMultiplicity'].upper">-->
-
-  <!--            <label for="endMultiLower" class="capitalized">{{ t("multiplicity_lower") }}</label>-->
-  <!--            <input id="endMultiLower" type="number" v-model="data.instance.parent['endMultiplicity'].lower">-->
-  <!--          </div>-->
-  <!--        </fieldset>-->
-  <!--      </template>-->
-  <!--    </template>-->
-  <!--  </fieldset>-->
 </template>
